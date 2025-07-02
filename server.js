0c5dfb56fc3867bd181ebddf74e52f2fd212d90f
JohnBartlett <JohnBartlett@users.noreply.github.com>
2025-07-02 11:18:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const HOST = '127.0.0.1';
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, HOST, () => {
-console.log(`🚀   Codespaces live-reload (what??) working at http://${HOST}:${PORT}`);
+console.log(`🚀   Codespaces live-reload (what??!!) working at http://${HOST}:${PORT}`);
 });
